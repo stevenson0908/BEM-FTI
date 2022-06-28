335508397c0bbf604b87b13b2f2ee2549aa67ee9
Stevenson <88644658+stevenson0908@users.noreply.github.com>
2022-06-28 20:27:55 +0700

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".team-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween:20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
